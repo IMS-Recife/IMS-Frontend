ea1fe58bc21fcbbdd134bdbcde8c755f31dd2f01
filipe Bessa <filipegbessa@gmail.com>
2021-02-14 00:58:56 -0300

    test default font

--- src/styles/global.js
+++ src/styles/global.js
@@ -10,5 +10,6 @@ export default createGlobalStyle`
     body {
         background: ${(props) => props.theme.colors.background};
         color: ${(props) => props.theme.colors.text};
+        font: 400 16px Roboto, sans-serif;
     }
 `;
