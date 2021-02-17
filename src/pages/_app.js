3f03a52e7bea2567248e8d8404bab956680ebe7c
filipe Bessa <filipegbessa@gmail.com>
2021-02-17 02:39:00 -0300

    Fix _app.js

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,4 +1,4 @@
-import PropTypes from "prop-types";
+/* eslint react/prop-types: 0 */
 import React from "react";
 import { ThemeProvider } from "styled-components";
 import I18nProvider from "../i18n/I18nProvider";
@@ -11,7 +11,7 @@ function MyApp({ Component, pageProps }) {
   return (
     <ThemeProvider theme={theme}>
       <I18nProvider>
-        <Component {...Component} {...pageProps} />
+        <Component {...pageProps} />
         <GlobalStyle />
       </I18nProvider>
     </ThemeProvider>
@@ -19,13 +19,3 @@ function MyApp({ Component, pageProps }) {
 }
 
 export default MyApp;
-
-MyApp.propTypes = {
-  Component: PropTypes.string,
-  pageProps: PropTypes.string,
-};
-
-MyApp.defaultProps = {
-  Component: "",
-  pageProps: "",
-};
