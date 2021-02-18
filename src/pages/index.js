bd4f97df09423dc9856ab45b3310ead7a2c681a3
filipe Bessa <filipegbessa@gmail.com>
2021-02-18 06:48:37 -0300

    Change Sidebar icons

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,7 +1,13 @@
 import Head from "next/head";
 import React from "react";
-import { ProSidebar, SubMenu, MenuItem, Menu } from "react-pro-sidebar";
-import { FaHeart, FaGem } from "react-icons/fa";
+import { ProSidebar, MenuItem, Menu } from "react-pro-sidebar";
+import {
+  FaGem,
+  FaChartBar,
+  FaChartLine,
+  FaMapMarkedAlt,
+  FaPlusSquare,
+} from "react-icons/fa";
 import Navbar from "../patterns/Navbar";
 
 export default function Home() {
@@ -13,12 +19,12 @@ export default function Home() {
       <Navbar />
       <ProSidebar>
         <Menu iconShape="square">
-          <SubMenu title="Components" icon={<FaGem />}>
-            <MenuItem>Component 1</MenuItem>
-            <SubMenu title="Sub Component 1" icon={<FaHeart />}>
-              {/* you can have more nested submenus ... */}
-            </SubMenu>
-          </SubMenu>
+          <MenuItem icon={<FaPlusSquare size="1.5rem" />}>Dashboard</MenuItem>
+          <MenuItem icon={<FaGem size="1.5rem" />}>Dashboard</MenuItem>
+          <MenuItem icon={<FaChartBar size="1.5rem" />}>Dashboard</MenuItem>
+          <MenuItem icon={<FaChartLine size="1.5rem" />}>Dashboard</MenuItem>
+          <MenuItem icon={<FaGem size="1.5rem" />}>Dashboard</MenuItem>
+          <MenuItem icon={<FaMapMarkedAlt size="1.5rem" />}>Dashboard</MenuItem>
         </Menu>
       </ProSidebar>
     </div>
