aef79bbcdc8494a1b0e9d5fd4aa7b0009a11a5cd
filipe Bessa <filipegbessa@gmail.com>
2021-02-23 15:43:37 -0300

    Add icons and their css

--- src/patterns/MobileToolbar/index.js
+++ src/patterns/MobileToolbar/index.js
@@ -1,24 +1,65 @@
 import React from "react";
 import AppBar from "@material-ui/core/AppBar";
 import Toolbar from "@material-ui/core/Toolbar";
-import Typography from "@material-ui/core/Typography";
-import Button from "@material-ui/core/Button";
 import IconButton from "@material-ui/core/IconButton";
 import {
-  // FaAngleLeft,
-  // FaAngleRight,
-  // FaBuffer,
-  // FaChartBar,
-  // FaChartLine,
-  // FaCity,
-  // FaMapMarkedAlt,
+  FaBuffer,
+  FaChartBar,
+  FaChartLine,
+  FaCity,
+  FaMapMarkedAlt,
   FaPlusSquare,
 } from "react-icons/fa";
 import styled from "styled-components";
 
 const StyledAppBar = styled(AppBar)`
   background: ${(props) => props.theme.colors.background} !important;
-  margin-top: 10px;
+  margin-top: 50px;
+`;
+
+const StyledFaPlusSquare = styled(FaPlusSquare)`
+  color: ${(props) => props.theme.colors.warning};
+
+  :hover {
+    color: ${(props) => props.theme.colors.light};
+  }
+`;
+
+const StyledFaBuffer = styled(FaBuffer)`
+  color: ${(props) => props.theme.colors.primary};
+
+  :hover {
+    color: ${(props) => props.theme.colors.light};
+  }
+`;
+
+const StyledFaChartBar = styled(FaChartBar)`
+  color: ${(props) => props.theme.colors.primary};
+
+  :hover {
+    color: ${(props) => props.theme.colors.light};
+  }
+`;
+const StyledFaChartLine = styled(FaChartLine)`
+  color: ${(props) => props.theme.colors.primary};
+
+  :hover {
+    color: ${(props) => props.theme.colors.light};
+  }
+`;
+const StyledFaCity = styled(FaCity)`
+  color: ${(props) => props.theme.colors.primary};
+
+  :hover {
+    color: ${(props) => props.theme.colors.light};
+  }
+`;
+const StyledFaMapMarkedAlt = styled(FaMapMarkedAlt)`
+  color: ${(props) => props.theme.colors.primary};
+
+  :hover {
+    color: ${(props) => props.theme.colors.light};
+  }
 `;
 
 export default function ButtonAppBar() {
@@ -27,10 +68,23 @@ export default function ButtonAppBar() {
       <StyledAppBar position="static">
         <Toolbar>
           <IconButton color="inherit" aria-label="menu">
-            <FaPlusSquare />
+            <StyledFaPlusSquare />
           </IconButton>
-          <Typography variant="h6">News</Typography>
-          <Button color="inherit">Login</Button>
+          <IconButton color="inherit" aria-label="menu">
+            <StyledFaBuffer />
+          </IconButton>
+          <IconButton color="inherit" aria-label="menu">
+            <StyledFaChartBar />
+          </IconButton>
+          <IconButton color="inherit" aria-label="menu">
+            <StyledFaChartLine />
+          </IconButton>
+          <IconButton color="inherit" aria-label="menu">
+            <StyledFaCity />
+          </IconButton>
+          <IconButton color="inherit" aria-label="menu">
+            <StyledFaMapMarkedAlt />
+          </IconButton>
         </Toolbar>
       </StyledAppBar>
     </div>
