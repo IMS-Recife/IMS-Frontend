d970cc33eab082cd9b8056b1c1f516d555036da1
filipe Bessa <filipegbessa@gmail.com>
2021-02-26 15:07:13 -0300

    Create MobileDashBoard component and make it disappear when the Bars button is clicked

--- src/pages/index.js
+++ src/pages/index.js
@@ -17,8 +17,8 @@ import {
 import useWindowDimensions from "../Hooks/useWindowDimensions";
 import MaterialUISelect from "../components/MaterialUISelect";
 import Navbar from "../patterns/Navbar";
-import MobileToolbar from "../patterns/MobileToolbar";
 import MobileHeaderMenu from "../patterns/MobileHeaderMenu";
+import MobileDashboard from "../patterns/MobileDashboard";
 
 const ArrowButton = styled.button`
   background: none;
@@ -65,235 +65,170 @@ function Home() {
       </Head>
       <Navbar showMobileMenu={handleShowMobileMenu} />
       <MobileHeaderMenu show={showMobileMenu} />
-      <a href="http://www.recife.pe.gov.br/" target="_blank" rel="noreferrer">
-        <img
-          src="/prefeiturarecife.png"
-          alt="prefeitura do recife"
-          width="70px"
-          height="70px"
-          className="rounded"
-          style={{
-            marginTop: "20px",
-            marginLeft: "40px",
-            display: "inline-block",
-          }}
-        />
-      </a>
-      <div
-        style={{
-          position: "absolute",
-          marginTop: "25px",
-          marginLeft: "20px",
-          display: "inline-block",
-        }}
-      >
-        <MaterialUISelect />
-      </div>
-
-      <div
-        style={{
-          display: "inline-block",
-          position: "absolute",
-          left: "50%",
-          transform: "translateX(-50%)",
-          marginTop: "25px",
-        }}
-        className="mt-5 mt-sm-3"
-      >
-        <span
-          style={{
-            font: "700 10px Roboto, sans-serif",
-            color: "#87c2ff",
-            display: "block",
-            textAlign: "center",
-          }}
-          className="mt-5 mt-sm-3"
-        >
-          <FormattedMessage id="LAST_UPDATE" />
-        </span>
-        <span
-          style={{
-            marginTop: "5px",
-            font: "500 18px Roboto, sans-serif",
-            display: "block",
-            textAlign: "center",
-          }}
-        >
-          03/01/2020 23h59
-        </span>
-      </div>
       {windowWidth <= 776 ? (
-        <div>
-          <MobileToolbar />
-          <div className="col-12 mt-3">
-            <div
-              className="card card-responsive p-5 pt-5 mr-sm-0 ml-md-4 m-lg-0"
-              style={{
-                marginBottom: "40px",
-                marginLeft: "0px",
-              }}
-            >
-              <div className="row">
-                <div
-                  className="card card-custom bg-secondary col-12 m-0 mb-3 col-lg-5 mr-md-2 m-xl-2 ml-xl-5 "
-                  style={{ height: "250px" }}
-                />
-                <div
-                  className="card card-custom bg-primary col-12 m-0 mb-3 col-lg-6  m-lg-0 ml-xl-2 mt-xl-2"
-                  style={{ height: "250px" }}
-                />
-                <div
-                  className="card card-custom bg-info col-12 m-0 mb-3 col-lg-4 mr-md-2 m-xl-2 ml-xl-5 "
-                  style={{ height: "300px" }}
-                />
-                <div
-                  className="card card-custom bg-success col-12 m-0 mb-3 col-lg-7  m-xl-2"
-                  style={{ height: "300px" }}
-                />
-                <div
-                  className="card card-custom bg-warning col-12 m-0 mb-3 col-lg-4 mr-md-2 m-xl-2 ml-xl-5 mb-xl-4 "
-                  style={{ height: "300px" }}
-                />
-                <div
-                  className="card card-custom bg-danger col-12 m-0 mb-3 col-lg-7  m-xl-2 mb-4"
-                  style={{ height: "300px" }}
-                />
-              </div>
-            </div>
-          </div>
-        </div>
+        <MobileDashboard show={!showMobileMenu} />
       ) : (
-        <Container fluid>
-          <div className="row mt-3">
-            <div className={sideBarClassName}>
-              <ProSidebar
-                style={{ marginLeft: "35px", marginTop: "0px" }}
-                collapsed={collapseSideBar}
-              >
-                <Menu iconShape="square">
-                  <ArrowButton
-                    type="button"
-                    onClick={() => setCollapseSideBar(!collapseSideBar)}
-                  >
+        <div>
+          <a
+            href="http://www.recife.pe.gov.br/"
+            target="_blank"
+            rel="noreferrer"
+          >
+            <img
+              src="/prefeiturarecife.png"
+              alt="prefeitura do recife"
+              width="70px"
+              height="70px"
+              className="rounded"
+              style={{
+                marginTop: "20px",
+                marginLeft: "40px",
+                display: "inline-block",
+              }}
+            />
+          </a>
+          <div
+            style={{
+              position: "absolute",
+              marginTop: "25px",
+              marginLeft: "20px",
+              display: "inline-block",
+            }}
+          >
+            <MaterialUISelect />
+          </div>
+          <Container fluid>
+            <div className="row mt-3">
+              <div className={sideBarClassName}>
+                <ProSidebar
+                  style={{ marginLeft: "35px", marginTop: "0px" }}
+                  collapsed={collapseSideBar}
+                >
+                  <Menu iconShape="square">
+                    <ArrowButton
+                      type="button"
+                      onClick={() => setCollapseSideBar(!collapseSideBar)}
+                    >
+                      <MenuItem
+                        style={{ color: "#FFFFFF", marginTop: "20px" }}
+                        icon={
+                          collapseSideBar ? (
+                            <FaAngleRight size="2rem" />
+                          ) : (
+                            <FaAngleLeft size="2rem" />
+                          )
+                        }
+                      />
+                    </ArrowButton>
                     <MenuItem
-                      style={{ color: "#FFFFFF", marginTop: "20px" }}
+                      className="mt-2"
+                      style={{ color: "#FEEC47" }}
                       icon={
-                        collapseSideBar ? (
-                          <FaAngleRight size="2rem" />
-                        ) : (
-                          <FaAngleLeft size="2rem" />
-                        )
+                        <FaPlusSquare
+                          size="1.8rem"
+                          title={intl.formatMessage({ id: "ADD_INDICATOR" })}
+                        />
                       }
+                    >
+                      <FormattedMessage id="ADD_INDICATOR" />
+                    </MenuItem>
+                    <MenuItem
+                      className="mt-2"
+                      icon={
+                        <FaBuffer
+                          size="1.8rem"
+                          title={intl.formatMessage({ id: "SHOW_ALL" })}
+                        />
+                      }
+                    >
+                      <FormattedMessage id="SHOW_ALL" />
+                    </MenuItem>
+                    <MenuItem
+                      className="mt-2"
+                      icon={
+                        <FaChartBar
+                          size="1.8rem"
+                          title={intl.formatMessage({ id: "ANALYSIS" })}
+                        />
+                      }
+                    >
+                      <FormattedMessage id="ANALYSIS" />
+                    </MenuItem>
+                    <MenuItem
+                      className="mt-2"
+                      icon={
+                        <FaChartLine
+                          size="1.8rem"
+                          title={intl.formatMessage({ id: "SIRD" })}
+                        />
+                      }
+                    >
+                      <FormattedMessage id="SIRD" />
+                    </MenuItem>
+                    <MenuItem
+                      className="mt-2"
+                      icon={
+                        <FaCity
+                          size="1.8rem"
+                          title={intl.formatMessage({ id: "NEIGHBORHOODS" })}
+                        />
+                      }
+                    >
+                      <FormattedMessage id="NEIGHBORHOODS" />
+                    </MenuItem>
+                    <MenuItem
+                      className="mt-2"
+                      icon={
+                        <FaMapMarkedAlt
+                          size="1.8rem"
+                          title={intl.formatMessage({ id: "MAP" })}
+                        />
+                      }
+                    >
+                      <FormattedMessage id="MAP" />
+                    </MenuItem>
+                  </Menu>
+                </ProSidebar>
+              </div>
+              <div className={bigCardClassName}>
+                <div
+                  className="card card-responsive p-5 pt-5 mr-sm-0 ml-md-4 m-lg-0"
+                  style={{
+                    marginBottom: "40px",
+                    marginLeft: "0px",
+                  }}
+                >
+                  <div className="row">
+                    <div
+                      className="card card-custom bg-secondary col-12 m-0 mb-3 col-lg-5 mr-md-2 m-xl-2 ml-xl-5 "
+                      style={{ height: "250px" }}
                     />
-                  </ArrowButton>
-                  <MenuItem
-                    className="mt-2"
-                    style={{ color: "#FEEC47" }}
-                    icon={
-                      <FaPlusSquare
-                        size="1.8rem"
-                        title={intl.formatMessage({ id: "ADD_INDICATOR" })}
-                      />
-                    }
-                  >
-                    <FormattedMessage id="ADD_INDICATOR" />
-                  </MenuItem>
-                  <MenuItem
-                    className="mt-2"
-                    icon={
-                      <FaBuffer
-                        size="1.8rem"
-                        title={intl.formatMessage({ id: "SHOW_ALL" })}
-                      />
-                    }
-                  >
-                    <FormattedMessage id="SHOW_ALL" />
-                  </MenuItem>
-                  <MenuItem
-                    className="mt-2"
-                    icon={
-                      <FaChartBar
-                        size="1.8rem"
-                        title={intl.formatMessage({ id: "ANALYSIS" })}
-                      />
-                    }
-                  >
-                    <FormattedMessage id="ANALYSIS" />
-                  </MenuItem>
-                  <MenuItem
-                    className="mt-2"
-                    icon={
-                      <FaChartLine
-                        size="1.8rem"
-                        title={intl.formatMessage({ id: "SIRD" })}
-                      />
-                    }
-                  >
-                    <FormattedMessage id="SIRD" />
-                  </MenuItem>
-                  <MenuItem
-                    className="mt-2"
-                    icon={
-                      <FaCity
-                        size="1.8rem"
-                        title={intl.formatMessage({ id: "NEIGHBORHOODS" })}
-                      />
-                    }
-                  >
-                    <FormattedMessage id="NEIGHBORHOODS" />
-                  </MenuItem>
-                  <MenuItem
-                    className="mt-2"
-                    icon={
-                      <FaMapMarkedAlt
-                        size="1.8rem"
-                        title={intl.formatMessage({ id: "MAP" })}
-                      />
-                    }
-                  >
-                    <FormattedMessage id="MAP" />
-                  </MenuItem>
-                </Menu>
-              </ProSidebar>
-            </div>
-            <div className={bigCardClassName}>
-              <div
-                className="card card-responsive p-5 pt-5 mr-sm-0 ml-md-4 m-lg-0"
-                style={{
-                  marginBottom: "40px",
-                  marginLeft: "0px",
-                }}
-              >
-                <div className="row">
-                  <div
-                    className="card card-custom bg-secondary col-12 m-0 mb-3 col-lg-5 mr-md-2 m-xl-2 ml-xl-5 "
-                    style={{ height: "250px" }}
-                  />
-                  <div
-                    className="card card-custom bg-primary col-12 m-0 mb-3 col-lg-6  m-lg-0 ml-xl-2 mt-xl-2"
-                    style={{ height: "250px" }}
-                  />
-                  <div
-                    className="card card-custom bg-info col-12 m-0 mb-3 col-lg-4 mr-md-2 m-xl-2 ml-xl-5 "
-                    style={{ height: "300px" }}
-                  />
-                  <div
-                    className="card card-custom bg-success col-12 m-0 mb-3 col-lg-7  m-xl-2"
-                    style={{ height: "300px" }}
-                  />
-                  <div
-                    className="card card-custom bg-warning col-12 m-0 mb-3 col-lg-4 mr-md-2 m-xl-2 ml-xl-5 mb-xl-4 "
-                    style={{ height: "300px" }}
-                  />
-                  <div
-                    className="card card-custom bg-danger col-12 m-0 mb-3 col-lg-7  m-xl-2 mb-4"
-                    style={{ height: "300px" }}
-                  />
+                    <div
+                      className="card card-custom bg-primary col-12 m-0 mb-3 col-lg-6  m-lg-0 ml-xl-2 mt-xl-2"
+                      style={{ height: "250px" }}
+                    />
+                    <div
+                      className="card card-custom bg-info col-12 m-0 mb-3 col-lg-4 mr-md-2 m-xl-2 ml-xl-5 "
+                      style={{ height: "300px" }}
+                    />
+                    <div
+                      className="card card-custom bg-success col-12 m-0 mb-3 col-lg-7  m-xl-2"
+                      style={{ height: "300px" }}
+                    />
+                    <div
+                      className="card card-custom bg-warning col-12 m-0 mb-3 col-lg-4 mr-md-2 m-xl-2 ml-xl-5 mb-xl-4 "
+                      style={{ height: "300px" }}
+                    />
+                    <div
+                      className="card card-custom bg-danger col-12 m-0 mb-3 col-lg-7  m-xl-2 mb-4"
+                      style={{ height: "300px" }}
+                    />
+                  </div>
                 </div>
               </div>
             </div>
-          </div>
-        </Container>
+          </Container>
+        </div>
       )}
     </div>
   );
