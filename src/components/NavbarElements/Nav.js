cf2fc6664a0a2aad196fae69274ee5f7f94b9dbc
filipe Bessa <filipegbessa@gmail.com>
2021-03-04 20:27:47 -0300

    Fix the Nav bug that happened when the City Select was clicked

--- src/components/NavbarElements/Nav.js
+++ src/components/NavbarElements/Nav.js
@@ -3,6 +3,7 @@ import styled from "styled-components";
 const Nav = styled.nav`
   background: #262f51;
   height: 52px;
+  width: 100vw;
   display: flex;
   z-index: 10;
   justify-content: flex-start;
