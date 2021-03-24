0217b2b08a5dd7c36d3e15c0f46cde5ed3c4955c
filipe Bessa <filipegbessa@gmail.com>
2021-03-24 11:38:49 -0300

    Add MotorVehicleRepairJobs chart in economy page and change the charts sizes and positions

--- src/pages/economy.js
+++ src/pages/economy.js
@@ -3,6 +3,7 @@ import React from "react";
 import Layout from "../components/Layout";
 import PIBPerCapta from "../components/EconomyCharts/PIBPerCapta";
 import ConstructionJobs from "../components/EconomyCharts/ConstructionJobs";
+import MotorVehicleRepairJobs from "../components/EconomyCharts/MotorVehicleRepairJobs";
 
 const CovidPage = () => (
   <Layout>
@@ -11,29 +12,26 @@ const CovidPage = () => (
       className="w-100 mx-4 mb-4 p-4 rounded"
     >
       <div className="grid grid-cols-1 gap-4 sm:grid-cols-3 md:grid-cols-4 xl:grid-cols-8 md:gap-8">
-        <div className="text-black-50 col-span-1 sm:col-span-3 md:col-span-4 xl:col-span-3 row-span-2 bg-white rounded-lg shadow-xl h-100 w-100">
-          <PIBPerCapta />
+        <div className="bg-white rounded-lg md:col-span-2 shadow-xl h-48">
+          1
         </div>
-        <div className="text-black-50 col-span-1 md:col-span-4 xl:col-span-3 row-span-2 bg-white rounded-lg shadow-xl h-100">
+        <div className="bg-white rounded-lg md:col-span-2 shadow-xl h-48">
+          1
+        </div>
+        <div className="bg-white rounded-lg md:col-span-2 shadow-xl h-48">
+          1
+        </div>
+        <div className="bg-white rounded-lg md:col-span-2 shadow-xl h-48">
+          1
+        </div>
+        <div className="text-black-50 col-span-1 md:col-span-4 xl:col-span-4 row-span-2 bg-white rounded-lg shadow-xl h-100">
           <ConstructionJobs />
         </div>
-        <div className="bg-white rounded-lg md:col-span-2 shadow-xl h-48">
-          1
+        <div className="text-black-50 col-span-1 md:col-span-4 xl:col-span-4 row-span-2 bg-white rounded-lg shadow-xl h-100">
+          <MotorVehicleRepairJobs />
         </div>
-        <div className="bg-white rounded-lg md:col-span-2 shadow-xl h-48">
-          1
-        </div>
-        <div className="bg-white rounded-lg md:col-span-2 shadow-xl h-48">
-          1
-        </div>
-        <div className="bg-white rounded-lg md:col-span-2 shadow-xl h-48">
-          1
-        </div>
-        <div className="bg-white rounded-lg md:col-span-2 shadow-xl h-48">
-          1
-        </div>
-        <div className="bg-white rounded-lg md:col-span-2 shadow-xl h-48">
-          1
+        <div className="text-black-50 col-span-1 sm:col-span-3 md:col-span-4 xl:col-span-8 row-span-2 bg-white rounded-lg shadow-xl h-100 w-100">
+          <PIBPerCapta />
         </div>
       </div>
     </div>
