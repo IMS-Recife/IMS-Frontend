d32f279599a20494417e9bd8db930bc48d0c5cbd
Filipegbessaa <filipegbessa@gmail.com>
2021-06-02 22:56:51 -0300

    Add all images in the homepage except in destaques

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,7 +1,28 @@
 import { injectIntl } from "react-intl";
 import React from "react";
 import Slider from "react-slick";
+import {
+  FaFacebookSquare,
+  FaGithubSquare,
+  FaTwitterSquare,
+  FaYoutubeSquare,
+  FaCity,
+} from "react-icons/fa";
+import { RiMoneyDollarCircleLine, RiVirusFill } from "react-icons/ri";
+import { BsFileEarmark } from "react-icons/bs";
 import Layout from "../components/Layout";
+import politians from "../assets/contentImages/politians.svg";
+import crowd from "../assets/contentImages/crowd.svg";
+import law from "../assets/contentImages/law.svg";
+import mapImage from "../assets/contentImages/mapImage.svg";
+import logoIMS from "../assets/logoIMS.svg";
+import {
+  ariesLogo,
+  gefLogo,
+  onuLogo,
+  portoDigitalLogo,
+  ministerioLogo,
+} from "../assets/organizationsLogos/index";
 
 const Home = () => {
   const settings = {
@@ -33,25 +54,25 @@ const Home = () => {
           </div>
           <div className="flex items-stretch justify-between mt-auto mb-2">
             <a
-              href="#"
+              href=" #"
               className="font-medium font-bold uppercase text-gray-500 hover:text-gray-900 hover:no-underline"
             >
               Sobre o IMS
             </a>
             <a
-              href="#"
+              href=" #"
               className="font-medium font-bold uppercase text-gray-500 hover:text-gray-900 hover:no-underline"
             >
               Destaques
             </a>
             <a
-              href="#"
+              href=" #"
               className="font-medium font-bold uppercase text-gray-500 hover:text-gray-900 hover:no-underline"
             >
               Contato
             </a>
             <a
-              href="#"
+              href=" #"
               className="font-medium font-bold uppercase text-gray-500 hover:text-gray-900 hover:no-underline"
             >
               Parceiros
@@ -63,21 +84,24 @@ const Home = () => {
         className="flex"
         style={{
           height: "680px",
-          backgroundImage: `url("https://via.placeholder.com/1836x672")`,
+          backgroundImage: `url(${mapImage})`,
         }}
       >
         <div className="my-auto ml-48 w-3/6">
           <h1 className="font-bold text-5xl mb-4 text-left text-primary-text">
             Planejamento Urbano Integrado baseado em evidências
           </h1>
-          <button className="bg-secondary-green px-4 py-3 text-lg font-semibold uppercase text-white rounded-full hover:bg-secondary-green-darker">
+          <button
+            type="button"
+            className="bg-secondary-green px-4 py-3 text-lg font-semibold uppercase text-white rounded-full hover:bg-secondary-green-darker"
+          >
             Explorar o Mapa
           </button>
         </div>
       </section>
       <section className="container py-8 flex">
-        <div className="w-full h-96 mr-16 flex items-center border border-red-300"></div>
-        <div className="border border-red-300">
+        <img src={logoIMS} height="135px" width="135px" alt="logoIMS" />
+        <div className="border border-red-300 ml-5">
           <h1 className="font-bold text-4xl uppercase mb-4 text-left text-primary-text">
             Sobre o IMS
           </h1>
@@ -104,7 +128,10 @@ const Home = () => {
               (PNUMA) e execução de instituições parceiras.
             </p>
             <div className="mx-auto mt-8">
-              <button className="bg-secondary-green px-4 py-3 text-lg font-semibold uppercase text-white rounded-full hover:bg-secondary-green-darker">
+              <button
+                type="button"
+                className="bg-secondary-green px-4 py-3 text-lg font-semibold uppercase text-white rounded-full hover:bg-secondary-green-darker"
+              >
                 SAIBA MAIS SOBRE O CITINOVA
               </button>
             </div>
@@ -117,27 +144,27 @@ const Home = () => {
         </h1>
         <div className="grid grid-cols-3 border border-red-300">
           <div className="flex items-center flex-col">
-            <div className="w-32 h-32 bg-red-300" />
+            <img src={politians} alt="Políticos" className="w-32 h-32" />
             <span className="text-lg">Poder Executivo</span>
-            <p className="text-base">
+            <p className="text-base mt-4 text-justify">
               Prefeitos/as, secretariado, gerentes e técnicos terão seus
               trabalhos facilitados pelo IMS ao atualizar ou acessar dados
               georreferenciados sobre a requalificação de passeios públicos de
               toda cidade de maneira integrada.
             </p>
           </div>
-          <div className="flex items-center flex-col">
-            <div className="w-32 h-32 bg-red-300" />
+          <div className="flex items-center flex-col ml-4">
+            <img src={law} alt="Lei" className="w-32 h-32" />
             <span className="text-lg">Poder Legislativo</span>
-            <p className="text-base">
+            <p className="text-base mt-4 text-justify">
               Vereadores e suas equipes poderão utilizar o IMS em seu trabalho
               de supervisão do executivo, por exemplo.
             </p>
           </div>
-          <div className="flex items-center flex-col">
-            <div className="w-32 h-32 bg-red-300" />
+          <div className="flex items-center flex-col ml-4">
+            <img src={crowd} alt="pessoas" className="w-32 h-32" />
             <span className="text-lg">Sociedade Civil</span>
-            <p className="text-base">
+            <p className="text-base mt-4 text-justify">
               Membros da sociedade civil em geral terão à sua disposição uma
               plataforma de controle social e participação na qual será possível
               acessar de maneira conjunta dados sobre a requalificação de
@@ -151,21 +178,33 @@ const Home = () => {
           MACROÁREAS
         </h1>
         <div className="grid grid-cols-4 border border-red-300 gap-4">
-          <div className="bg-secondary-green h-48 rounded-lg">Planos</div>
-          <div className="bg-secondary-green h-48 rounded-lg">Projetos</div>
-          <div className="bg-secondary-green h-48 rounded-lg">Economia</div>
-          <div className="bg-secondary-green h-48 rounded-lg">Covid</div>
+          <div className="bg-secondary-green h-48 w-48 rounded-lg flex flex-column items-center justify-center">
+            <BsFileEarmark color="white" size="7rem" />
+            <p className="text-white text-md font-bold mt-2">Planos</p>
+          </div>
+          <div className="bg-secondary-green h-48 w-48 rounded-lg flex flex-column items-center justify-center">
+            <FaCity color="white" size="7rem" />
+            <p className="text-white text-md font-bold mt-2">Projetos</p>
+          </div>
+          <div className="bg-secondary-green h-48 w-48 rounded-lg flex flex-column items-center justify-center">
+            <RiMoneyDollarCircleLine color="white" size="7rem" />
+            <p className="text-white text-md font-bold mt-2">Economia</p>
+          </div>
+          <div className="bg-secondary-green h-48 w-48 rounded-lg flex flex-column items-center justify-center">
+            <RiVirusFill color="white" size="7rem" />
+            <p className="text-white text-md font-bold mt-2">Covid-19</p>
+          </div>
         </div>
       </section>
       <section className="container border border-red-300">
         <h1 className="font-bold text-4xl uppercase mb-4 text-left text-primary-text">
           Nossas Redes
         </h1>
-        <div className="grid grid-cols-4 border border-red-300 gap-4">
-          <div className="bg-secondary-green h-48 rounded-lg">Twitter</div>
-          <div className="bg-secondary-green h-48 rounded-lg">Facebook</div>
-          <div className="bg-secondary-green h-48 rounded-lg">Youtube</div>
-          <div className="bg-secondary-green h-48 rounded-lg">Github</div>
+        <div className="grid grid-cols-4 border border-red-300 gap-1 mx-5">
+          <FaTwitterSquare className="text-secondary-green" size="8rem" />
+          <FaFacebookSquare className="text-secondary-green" size="8rem" />
+          <FaYoutubeSquare className="text-secondary-green" size="8rem" />
+          <FaGithubSquare className="text-secondary-green" size="8rem" />
         </div>
       </section>
       <section className="container border border-red-300 bg-primary-gray my-12">
@@ -175,7 +214,10 @@ const Home = () => {
         <div className="border-b border-black p-8">
           <Slider {...settings}>
             <div>
-              <img src="https://via.placeholder.com/1300x450" />
+              <img
+                src="https://via.placeholder.com/1300x450"
+                alt="placeholder"
+              />
               <span>00 mês, 2021</span>
               <p>
                 Slap the dog because cats rule mice yet dead stare with ears
@@ -184,7 +226,10 @@ const Home = () => {
               </p>
             </div>
             <div>
-              <img src="https://via.placeholder.com/1300x450" />
+              <img
+                src="https://via.placeholder.com/1300x450"
+                alt="placeholder"
+              />
               <span>00 mês, 2021</span>
               <p>
                 Slap the dog because cats rule mice yet dead stare with ears
@@ -193,7 +238,10 @@ const Home = () => {
               </p>
             </div>
             <div>
-              <img src="https://via.placeholder.com/1300x450" />
+              <img
+                src="https://via.placeholder.com/1300x450"
+                alt="placeholder"
+              />
               <span>00 mês, 2021</span>
               <p>
                 Slap the dog because cats rule mice yet dead stare with ears
@@ -202,7 +250,10 @@ const Home = () => {
               </p>
             </div>
             <div>
-              <img src="https://via.placeholder.com/1300x450" />
+              <img
+                src="https://via.placeholder.com/1300x450"
+                alt="placeholder"
+              />
               <span>00 mês, 2021</span>
               <p>
                 Slap the dog because cats rule mice yet dead stare with ears
@@ -214,7 +265,7 @@ const Home = () => {
         </div>
         <div className="grid grid-cols-3 border border-red-300 my-8 gap-4 p-8">
           <div>
-            <img src="https://via.placeholder.com/420x170" />
+            <img src="https://via.placeholder.com/420x170" alt="placeholder" />
             <span>00 mês, 2021</span>
             <p>
               Slap the dog because cats rule mice yet dead stare with ears
@@ -223,7 +274,7 @@ const Home = () => {
             </p>
           </div>
           <div>
-            <img src="https://via.placeholder.com/420x170" />
+            <img src="https://via.placeholder.com/420x170" alt="placeholder" />
             <span>00 mês, 2021</span>
             <p>
               Slap the dog because cats rule mice yet dead stare with ears
@@ -232,7 +283,7 @@ const Home = () => {
             </p>
           </div>
           <div>
-            <img src="https://via.placeholder.com/420x170" />
+            <img src="https://via.placeholder.com/420x170" alt="placeholder" />
             <span>00 mês, 2021</span>
             <p>
               Slap the dog because cats rule mice yet dead stare with ears
@@ -240,6 +291,18 @@ const Home = () => {
               windshield.
             </p>
           </div>
+        </div>
+      </section>
+      <section className="container border border-red-300 my-12">
+        <div className="flex flex-row flex-wrap space-x-5 space-y-5">
+          <img src={gefLogo} alt="gef" />
+          <img src={onuLogo} alt="ONU" />
+          <img
+            src={ministerioLogo}
+            alt="Ministério da Ciência, Tecnologia e Inovações"
+          />
+          <img src={ariesLogo} alt="Aries" />
+          <img src={portoDigitalLogo} alt="Porto Digital" />
         </div>
       </section>
     </Layout>
